c5889f34b4dde044780b66098f01a3cbcd42f2d3
jerryhuang3 <jerryhuang@utexas.edu>
2019-10-01 23:05:53 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+  const [input, setInput] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const changeInput = e => {
+    setInput(e);
+  };
+
+  const addGoal = () => {
+    console.log(input);
+    setGoals(goals => [
+      ...goals,
+      { id: Math.random().toString(), value: input }
+    ]);
+    setInput("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Press this button!" onPress={() => setOutputText('The text has changed')} />
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.text}
+          onChangeText={changeInput}
+          value={input}
+        />
+
+        <Button title="ADD" onPress={addGoal} />
+      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={goals}
+        renderItem={itemData => (
+          <View style={styles.listGoal}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main: {
+    padding: 50
   },
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  text: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  },
+  listGoal: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
+  }
 });
